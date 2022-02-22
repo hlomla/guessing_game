874455a35214ab79fc50facbc6076408a37c9e92
unknown <hlomla.tapuko@gmail.com>
2022-02-22 16:43:51 +0200

    new game started flashes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const number_guess = document.querySelector(".no_guessed");
 const guess_btn = document.querySelector(".g_btn");
 const msg = document.querySelector(".popup_msg");
+const flashMsg = document.querySelector(".flash_msg")
 
 let randomNumber = Math.ceil((Math.random() * 100))
 
@@ -8,15 +9,23 @@ function clickGuessBtn() {
     let guessedNo = Number(number_guess.value)
 
     if (guessedNo < randomNumber) {
-        msg.innerHTML = "Your guess is too low"
+        msg.innerHTML = "Your guess is too low";
     } else if (guessedNo > randomNumber) {
         msg.innerHTML = "Your guess is too high";
     } else {
         msg.innerHTML = `Correct, the secret number is ${guessedNo}`;
+        updateRandomNo = Math.ceil((Math.random() * 100));
+        flashMsg.innerHTML = flashMessage();
     }
     setTimeout(function () {
         msg.innerHTML = "";
     }, 3000);
 }
+function flashMessage(){
+
+        return "New game started!!!";  
+}
 
 guess_btn.addEventListener('click', clickGuessBtn)
+
+    
